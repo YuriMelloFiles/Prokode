4572318df8a77736080f26035e394df0cc305e5f
BrunoNascimento02 <122832117+BrunoNascimento02@users.noreply.github.com>
2023-10-29 18:38:15 -0300

    Update login.php

--- SiteProkode/login.php
+++ SiteProkode/login.php
@@ -106,78 +106,20 @@ session_destroy();
       <div class="container ">
         <div class="heading_container heading_center">
           <h2>
-            Our <span>Services</span>
+            Fazer <span>Login</span>
           </h2>
           <p>
-            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
+            Nessa sessão somente os administradores podem acessar <br>
+            Caso tente algumas vezes e não consiga seu sinal será bloqueado com essa página. 
           </p>
         </div>
-        <div class="row">
-          <div class="col-md-4 ">
-            <div class="box ">
-              <div class="img-box">
-                <img src="images/s1.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Currency Wallet
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                  The
-                  point of using
-                </p>
-                <a href="">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 ">
-            <div class="box ">
-              <div class="img-box">
-                <img src="images/s2.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Security Storage
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                  The
-                  point of using
-                </p>
-                <a href="">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 ">
-            <div class="box ">
-              <div class="img-box">
-                <img src="images/s3.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Expert Support
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                  The
-                  point of using
-                </p>
-                <a href="">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
+        
         </div>
-        <div class="btn-box">
-          <a href="">
-            View All
-          </a>
+            <form action="#" method="post">
+                <input type="text" name="nome" id="idnome" placeholder="Digite seu login"><br>
+                <input type="password" name="senha" id="idsenha" placeholder="Digite sua senha"><br>
+                <button type="submit">Logar</button>
+            </form>
         </div>
       </div>
     </div>
@@ -284,7 +226,12 @@ session_destroy();
 
   <!-- footer section -->
   <section class="footer_section">
-        <h1>página de login</h1>
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span> All Rights Reserved By
+        <a href="https://html.design/">Free Html Templates</a>
+      </p>
+    </div>
   </section>
   <!-- footer section -->
 
